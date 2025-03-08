71afa57c1498729121301787694e07f69ea91a22
Jan Skála <j.skala@hotmail.com>
2025-03-08 18:37:23 +0100

    Fhir debug page

--- FHIR/Extensions/HumanNames.cs
+++ FHIR/Extensions/HumanNames.cs
@@ -7,6 +7,9 @@ public static class HumanNames
     public static IEnumerable<HumanName> FromString(string name) =>
         name.Split().Select(x => new HumanName {Text = x});
 
-    public static string ToString(this IEnumerable<HumanName> names) =>
-        string.Join(" ", names.Select(x => x.Text));
+    public static string ToFullName(this IEnumerable<HumanName> names)
+    {
+        var name = names.FirstOrDefault();
+        return name is not null ? string.Join(" ", name.Given) + " " + name.Family : "Unknown";
+    }
 }
