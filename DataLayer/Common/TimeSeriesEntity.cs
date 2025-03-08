2cc50a6aac9c71864255610d3a0fa3bae7546f23
Jan Skála <j.skala@hotmail.com>
2025-03-08 22:16:09 +0100

    feat: add FhirId property to TimeSeriesEntity and update model snapshot

--- DataLayer/Common/TimeSeriesEntity.cs
+++ DataLayer/Common/TimeSeriesEntity.cs
@@ -5,9 +5,8 @@ namespace DataLayer.Common;
 public abstract class TimeSeriesEntity : Entity
 {
     public required string ExternalId { get; set; }
-
     public required string UserId { get; set; }
     public ApplicationUser User { get; set; } = null!;
-
     public required DateTime Time { get; set; }
+    public string FhirId { get; set; } = "";
 }
---
+++ DataLayer/Migrations/20250308211518_AddFhirId.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFhirId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FhirId",
+                table: "CarbsValues",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FhirId",
+                table: "BolusValues",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FhirId",
+                table: "BgValues",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FhirId",
+                table: "CarbsValues");
+
+            migrationBuilder.DropColumn(
+                name: "FhirId",
+                table: "BolusValues");
+
+            migrationBuilder.DropColumn(
+                name: "FhirId",
+                table: "BgValues");
+        }
+    }
+}
