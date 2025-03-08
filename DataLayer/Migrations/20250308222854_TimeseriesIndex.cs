2e519ce5831e764c84616bf473fce3c203386c18
Jan Skála <j.skala@hotmail.com>
2025-03-08 23:39:46 +0100

    feat: add index on Time property in TimeSeriesEntity and update model snapshot

---
+++ DataLayer/Migrations/20250308222854_TimeseriesIndex.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class TimeseriesIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CarbsValues_Time",
+                table: "CarbsValues",
+                column: "Time");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BolusValues_Time",
+                table: "BolusValues",
+                column: "Time");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BgValues_Time",
+                table: "BgValues",
+                column: "Time");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CarbsValues_Time",
+                table: "CarbsValues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BolusValues_Time",
+                table: "BolusValues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BgValues_Time",
+                table: "BgValues");
+        }
+    }
+}
